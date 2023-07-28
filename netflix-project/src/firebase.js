977b843408c6c5a6325a8aabfa26dd0cfbc70686
youva <kadji_y@etna-alternance.net>
2023-07-28 16:42:31 +0200

    signup and signin added

--- netflix-project/src/firebase.js
+++ netflix-project/src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyALYrHPVWsaArVtgysfJgEsVzVQoyaVbEY",
@@ -8,4 +10,11 @@ export const firebaseConfig = {
   messagingSenderId: "820854933904",
   appId: "1:820854933904:web:0efe7ab10f33d414940b15",
   measurementId: "G-H28X080D48"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
